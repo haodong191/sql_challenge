d87f01f8793de01f7ad760b7fdc9342bb683cdcc
Hao Dong <hdong215@gmail.com>
2021-08-13 16:31:26 -0400

    Update data_analisys.sql

--- EmployeeSQL/data_analisys.sql
+++ EmployeeSQL/data_analisys.sql
@@ -1,4 +1,4 @@
--- Data Analisys
+-- Data Analysis
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees 
 JOIN salaries on employees.emp_no = salaries.emp_no;
